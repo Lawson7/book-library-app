ebc4b098df6730ab87f9ab936295438d2690f40a
Harry Lawson <harryglawson@gmail.com>
2019-08-19 16:28:01 +1200

    Add and delete book feature

--- script.js
+++ script.js
@@ -10,9 +10,17 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary (Book){
     myLibrary.push(Book);
+    
 };
 
+const bookDisplay = document.getElementById('bookDisplay');
 
+
+function clearDisplay(){
+    bookDisplay.innerHTML = '';
+};
+
+//test books
 
 const theHobbit = new Book('The Hobbit', 'That G', '301 pages', 'Not Read');
 const theAlchemist = new Book('The Alchemist', 'Paul C', '212 pages', 'Read');
@@ -20,30 +28,54 @@ const theAlchemist = new Book('The Alchemist', 'Paul C', '212 pages', 'Read');
 addBookToLibrary(theAlchemist);
 addBookToLibrary(theHobbit);
 
+//test books
 
-const bookDisplay = document.getElementById('bookDisplay');
-
-function clearDisplay(){
-    myLibrary = [];
-}
 
 function render(books) {
+
     clearDisplay()
+
     books.forEach(book => {
+
         const bookBox = document.createElement('div');
-        const delButton = document.createElement('button');
+        const delButton = document.createElement('span');
+
+
         bookBox.className = 'bookBox'
         bookBox.textContent = '';
         bookDisplay.appendChild(bookBox);
+
         delButton.className = 'delBook'
-        delButton.textContent = 'Delete'
+        delButton.textContent = '×'
         bookDisplay.appendChild(delButton);
 
-        for (let prop in book){
-            bookBox.textContent = bookBox.textContent + (book[prop]) + '. ';
-        }
+        bookBox.setAttribute("order", books.indexOf(book));
+        console.log(bookBox.attributes.order)
+
+        delButton.setAttribute("order", books.indexOf(book));
+        console.log(delButton.attributes.order)
+    
+        delButton.addEventListener('click', function(e){
+
+            let order = e.target.order;
+
+            for (let i = 0; i < myLibrary.length; i++) {
+                if (myLibrary[i].order == order) {
+                    bookBox.remove();
+                    delButton.remove();
+                    console.log(myLibrary)
+                    myLibrary.splice(i, 1);
+                    break;
+                }
+            }
+        });
+
+            for (let prop in book){
+                bookBox.textContent = bookBox.textContent + (book[prop]) + '. ';
+            }
     });
-};
+}
+
 
 render(myLibrary);
 
@@ -55,4 +87,5 @@ function AddBookButton() {
     const newBook = new Book(title, author, pages + ' pages', read);
     addBookToLibrary(newBook);
     render(myLibrary);
+
 };
