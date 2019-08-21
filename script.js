f4b02ed5da1c4b271c70a4ce20eb711dc74972ed
Harry Lawson <harryglawson@gmail.com>
2019-08-21 17:28:47 +1200

    change read status feature added

--- script.js
+++ script.js
@@ -20,7 +20,9 @@ function clearDisplay(){
     bookDisplay.innerHTML = '';
 };
 
-//test books
+
+
+//TEST BOOKS
 
 const theHobbit = new Book('The Hobbit', 'That G', 'Fiction', 'Not Read');
 const theAlchemist = new Book('The Alchemist', 'Paul C', 'Fiction', 'Read');
@@ -28,7 +30,8 @@ const theAlchemist = new Book('The Alchemist', 'Paul C', 'Fiction', 'Read');
 addBookToLibrary(theAlchemist);
 addBookToLibrary(theHobbit);
 
-//test books
+//TEST BOOKS
+
 
 
 function render(books) {
@@ -38,30 +41,35 @@ function render(books) {
     books.forEach(book => {
 
         const bookCard = document.createElement('div')
-        const bookBox = document.createElement('div');
+        const bookBox = document.createElement('p');
+        const readIcon = document.createElement('span')
         const delButton = document.createElement('span');
 
         bookCard.className = 'bookCard';
         bookDisplay.appendChild(bookCard);
 
         bookBox.className = 'bookBox'
-        bookBox.textContent = '';
+        bookBox.textContent = book.title + ', ' + book.author + ', ' + book.genre + '.';
         bookCard.appendChild(bookBox);
+
+        readIcon.className = 'readIcon'
+        readIcon.textContent = book.read
+        bookCard.appendChild(readIcon);
 
         delButton.className = 'delBook'
         delButton.textContent = '🗑'
         bookCard.appendChild(delButton);
 
-        bookBox.setAttribute("order", books.indexOf(book));
+        bookBox.setAttribute("idNum", books.indexOf(book));
 
-        delButton.setAttribute("order", books.indexOf(book));
+        delButton.setAttribute("idNum", books.indexOf(book));
     
         delButton.addEventListener('click', function(e){
 
-            let order = e.target.order;
+            let idNum = e.target.idNum;
 
             for (let i = 0; i < myLibrary.length; i++) {
-                if (myLibrary[i].order == order) {
+                if (myLibrary[i].idNum == idNum) {
                     bookCard.remove();
                     console.log(myLibrary);
                     myLibrary.splice(i, 1);
@@ -70,24 +78,38 @@ function render(books) {
             }
         });
 
-            for (let prop in book){
-                bookBox.textContent = bookBox.textContent + (book[prop]) + '. ';
+        readIcon.addEventListener('click', function(e){
+            const readStatus = e.target.textContent
+            
+            if (readStatus == 'Read'){
+                e.target.textContent = 'Not Read';
+                book.read = e.target.textContent;
+
+            } else {
+                e.target.textContent = 'Read';
+                book.read = e.target.textContent;
             }
+
+        });
+
     });
 }
-
 
 render(myLibrary);
 
 function AddBookButton() {
 
+    if (addForm.title.value == '' || addForm.author.value == '' || addForm.genre.value == ''){
+        alert('Please enter correctly')
+    } else {
     const newBook = new Book(
-        addForm.name.value,
+        addForm.title.value,
         addForm.author.value,
         addForm.genre.value,
         addForm.read.value);
 
     addBookToLibrary(newBook);
     render(myLibrary);
-
+    addForm.reset();
+    }
 };
